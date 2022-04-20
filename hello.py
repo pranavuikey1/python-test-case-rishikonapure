e9d162aca6caa87f2608583dd7bcf28712a0a53a
Rushikesh Konapure <konapurer@gmail.com>
2022-04-20 13:10:57 +0530

    third submit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def hello_world():
     # Complete the function definition by adding tour code such that the function will return "Hello World!"
     
-    return 'Hello World'
+    return 'Hello World!'
